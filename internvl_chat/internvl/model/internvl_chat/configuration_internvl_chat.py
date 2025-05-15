6d9df1d4f184036f5fde6bfa9ed150d445db153d
Tran Hoang Anh <anhtranhoang081199@gmail.com>
2025-05-15 14:46:23 +0700

    fix

--- internvl_chat/internvl/model/internvl_chat/configuration_internvl_chat.py
+++ internvl_chat/internvl/model/internvl_chat/configuration_internvl_chat.py
@@ -22,22 +22,22 @@ class InternVLChatConfig(PretrainedConfig):
     is_composition = True
 
     def __init__(
-            self,
-            vision_config=None,
-            llm_config=None,
-            use_backbone_lora=0,
-            use_llm_lora=0,
-            pad2square=False,
-            select_layer=-1,
-            force_image_size=None,
-            downsample_ratio=0.5,
-            template=None,
-            dynamic_image_size=False,
-            use_thumbnail=False,
-            ps_version='v1',
-            min_dynamic_patch=1,
-            max_dynamic_patch=6,
-            **kwargs):
+        self,
+        vision_config=None,
+        llm_config=None,
+        use_backbone_lora=0,
+        use_llm_lora=0,
+        pad2square=False,
+        select_layer=-1,
+        force_image_size=None,
+        downsample_ratio=0.5,
+        template=None,
+        dynamic_image_size=False,
+        use_thumbnail=False,
+        ps_version='v1',
+        min_dynamic_patch=1,
+        max_dynamic_patch=6,
+        **kwargs):
         super().__init__(**kwargs)
 
         if vision_config is None:
@@ -46,19 +46,24 @@ class InternVLChatConfig(PretrainedConfig):
 
         if llm_config is None:
             llm_config = {}
-            logger.info('llm_config is None. Initializing the LlamaConfig config with default values (`LlamaConfig`).')
+            logger.info('llm_config is None. Initializing with default LlamaConfig.')
 
         self.vision_config = InternVisionConfig(**vision_config)
-        if llm_config['architectures'][0] == 'LlamaForCausalLM':
+
+        # Get architecture safely
+        arch = llm_config.get('architectures', ['LlamaForCausalLM'])[0]
+
+        if arch == 'LlamaForCausalLM':
             self.llm_config = LlamaConfig(**llm_config)
-        elif llm_config['architectures'][0] == 'InternLM2ForCausalLM':
+        elif arch == 'InternLM2ForCausalLM':
             self.llm_config = InternLM2Config(**llm_config)
-        elif llm_config['architectures'][0] == 'Phi3ForCausalLM':
+        elif arch == 'Phi3ForCausalLM':
             self.llm_config = Phi3Config(**llm_config)
-        elif llm_config['architectures'][0] == 'Qwen2ForCausalLM':
+        elif arch == 'Qwen2ForCausalLM':
             self.llm_config = Qwen2Config(**llm_config)
         else:
-            raise ValueError('Unsupported architecture: {}'.format(llm_config['architectures'][0]))
+            raise ValueError(f'Unsupported architecture: {arch}')
+
         self.use_backbone_lora = use_backbone_lora
         self.use_llm_lora = use_llm_lora
         self.pad2square = pad2square
@@ -68,7 +73,7 @@ class InternVLChatConfig(PretrainedConfig):
         self.template = template
         self.dynamic_image_size = dynamic_image_size
         self.use_thumbnail = use_thumbnail
-        self.ps_version = ps_version  # pixel shuffle version
+        self.ps_version = ps_version
         self.min_dynamic_patch = min_dynamic_patch
         self.max_dynamic_patch = max_dynamic_patch
 
@@ -76,6 +81,7 @@ class InternVLChatConfig(PretrainedConfig):
         logger.info(f'ps_version: {self.ps_version}')
         logger.info(f'min_dynamic_patch: {self.min_dynamic_patch}')
         logger.info(f'max_dynamic_patch: {self.max_dynamic_patch}')
+
 
     def to_dict(self):
         """
